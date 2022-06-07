754c6cfec0e691944c7c993e17d3614f9e0bc90c
Alexey M. Pereverzev <boss.pahan@gmail.com>
2022-06-07 16:14:58 +0300

    Добавление стилей цветов

---
+++ course_project/lets_cooking/lib/constants/colors.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/painting.dart';
+
+abstract class AppColors {
+  static const neutral100 = Color.fromRGBO(24, 24, 24, 1);
+  static const neutral90 = Color.fromRGBO(48, 48, 48, 1);
+  static const neutral80 = Color.fromRGBO(72, 72, 72, 1);
+  static const neutral70 = Color.fromRGBO(96, 96, 96, 1);
+  static const neutral60 = Color.fromRGBO(121, 121, 121, 1);
+  static const neutral50 = Color.fromRGBO(145, 145, 145, 1);
+  static const neutral40 = Color.fromRGBO(169, 169, 169, 1);
+  static const neutral30 = Color.fromRGBO(193, 193, 193, 1);
+  static const neutral20 = Color.fromRGBO(217, 217, 217, 1);
+  static const neutral10 = Color.fromRGBO(241, 241, 241, 1);
+
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+
+  static const primary100 = Color.fromRGBO(113, 31, 31, 1);
+  static const primary90 = Color.fromRGBO(136, 37, 37, 1);
+  static const primary80 = Color.fromRGBO(158, 43, 43, 1);
+  static const primary70 = Color.fromRGBO(181, 50, 50, 1);
+  static const primary60 = Color.fromRGBO(203, 56, 56, 1);
+  static const primary50 = Color.fromRGBO(226, 62, 62, 1);
+  static const primary40 = Color.fromRGBO(232, 101, 101, 1);
+  static const primary30 = Color.fromRGBO(238, 139, 139, 1);
+  static const primary20 = Color.fromRGBO(243, 178, 178, 1);
+  static const primary10 = Color.fromRGBO(249, 216, 216, 1);
+  static const primary0 = Color.fromRGBO(252, 236, 236, 1);
+
+  static const secondary100 = Color.fromRGBO(128, 78, 0, 1);
+  static const secondary90 = Color.fromRGBO(153, 94, 0, 1);
+  static const secondary80 = Color.fromRGBO(179, 109, 0, 1);
+  static const secondary70 = Color.fromRGBO(204, 125, 0, 1);
+  static const secondary60 = Color.fromRGBO(230, 140, 0, 1);
+  static const secondary50 = Color.fromRGBO(255, 156, 0, 1);
+  static const secondary40 = Color.fromRGBO(255, 166, 26, 1);
+  static const secondary30 = Color.fromRGBO(255, 186, 77, 1);
+  static const secondary20 = Color.fromRGBO(255, 206, 128, 1);
+  static const secondary10 = Color.fromRGBO(255, 225, 179, 1);
+  static const secondary0 = Color.fromRGBO(255, 245, 230, 1);
+
+  static const rating100 = Color.fromRGBO(255, 182, 97, 1);
+
+  static const error100 = Color.fromRGBO(238, 17, 51, 1);
+  static const error10 = Color.fromRGBO(253, 231, 235, 1);
+
+  static const success100 = Color.fromRGBO(49, 176, 87, 1);
+  static const success10 = Color.fromRGBO(206, 236, 215, 1);
+}
